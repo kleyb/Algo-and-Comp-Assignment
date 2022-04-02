a2cd30a4fe49c8da78afa8b971603d1ff88ec74f
kleyb <76008055+kleyb@users.noreply.github.com>
2022-04-02 22:51:47 +0100

    Add project files.

---
+++ Algo and Comp Assignment/Input.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+    
+class Input
+{
+
+     public int[] ReadFiles()
+     {
+        Console.WriteLine("Please indicate the name of the file and extersion (eg 'text.txt'): ");
+        string fileName = Console.ReadLine();
+        Console.WriteLine(@"Please indicate the path to the file: (eg 'C:\Users\kleybson\Download' ) ");
+        string path = Console.ReadLine();
+        
+        string[] arrayAsText = File.ReadAllLines(path + fileName);
+        //Convert array to Int    
+        int[] array = Array.ConvertAll(arrayAsText,s => int.TryParse(s,out var x) ? x :-1 );
+        return array;
+    }
+}       
+
+
---
+++ Algo and Comp Assignment/Program.cs
@@ -0,0 +1,38 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Welcome to the Shares Searching and Sorting Application");
+
+Input input = new Input();
+int[] share1 = input.ReadFiles();
+int[] share2 = input.ReadFiles();
+int[] share3 = input.ReadFiles();
+
+Console.WriteLine("Please select one of the options: ");
+Console.WriteLine(" 1 for sorting in acesding order");
+Console.WriteLine(" 2 for sorting in descending order");
+Console.WriteLine(" 3 to Search");
+Console.WriteLine(" 4 to Merge and Sort");
+
+while (true)
+{
+    if (Console.ReadLine() == "1")
+    {
+
+    }
+    else if (Console.ReadLine() == "2")
+    {
+
+    }
+    else if (Console.ReadLine() == "3")
+    {
+
+    }
+    else if (Console.ReadLine() == "4")
+    {
+
+    }
+    else
+    {
+        Console.WriteLine();
+    }
+}
